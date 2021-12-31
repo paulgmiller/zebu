060b3d5b461b03215384dd6528249300b3e1161b
paul.miller@gmail.com <paul.miller@gmail.com>
2021-12-30 17:50:59 -0800

    work with cloudflare backend

--- northbriton/main.go
+++ northbriton/main.go
@@ -13,44 +13,60 @@ import (
 	"github.com/cloudflare/cloudflare-go"
 )
 
-var names map[string]bool = map[string]bool{}
+var domainregex = regexp.MustCompile(`^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?$`)
+
+const nb = "northbriton.net"
 
 func main() {
 
-	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
-	if err != nil {
-		log.Fatalf("can't getcloudflare client %s", err)
-	}
-	nbid, err := api.ZoneIDByName("northbriton.net")
-	if err != nil {
-		log.Fatalf("can't get details %s", err)
-	}
-	fmt.Printf("trying to get records for %s\n", nbid)
-	records, err := api.DNSRecords(context.TODO(), nbid, cloudflare.DNSRecord{}) //Type: "TXT"
-	if err != nil {
-		log.Fatalf("can't get dns records %s", err)
-	}
-
-	for _, record := range records {
-		fmt.Printf(record.Name)
+	if names, err := listRecords(context.Background()); err != nil {
+		log.Fatalf(err.Error())
+	} else {
+		log.Printf("Starting with %d records", len(names))
 	}
 	// Set routing rules
 	http.HandleFunc("/reserve/", withLogging(Reserve))
 	http.HandleFunc("/list", withLogging(List))
 
 	//Use the default DefaultServeMux.
-	err = http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func listRecords(ctx context.Context) (map[string]string, error) {
+	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		return nil, fmt.Errorf("can't getcloudflare client %w", err)
+	}
+	nbid, err := api.ZoneIDByName(nb)
+	if err != nil {
+		return nil, fmt.Errorf("can't get zone %w", err)
+	}
+	records, err := api.DNSRecords(ctx, nbid, cloudflare.DNSRecord{Type: "TXT"})
+	if err != nil {
+		return nil, fmt.Errorf("can't get dns records %w", err)
+	}
+
+	result := map[string]string{}
+	for _, record := range records {
+		result[record.Name] = record.Content
+	}
+	return result, nil
+}
+
 func List(w http.ResponseWriter, r *http.Request) {
+	names, err := listRecords(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
-	log.Println("list request")
-	for name := range names {
-		fmt.Fprintln(w, name)
+	for name, value := range names {
+		fmt.Fprintln(w, name+"->"+value)
 	}
 }
 
@@ -63,15 +79,12 @@ func withLogging(h http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-var domainregex = regexp.MustCompile(`^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?$`)
-
 func Reserve(w http.ResponseWriter, r *http.Request) {
-	fmt.Println()
 	if r.Method != http.MethodPut {
 		http.Error(w, "only puts accepted", http.StatusBadRequest)
 		return
 	}
-	name := strings.TrimLeft(r.URL.Path, "/reserve/") //use http.StripPrefix?
+	name := strings.TrimPrefix(r.URL.Path, "/reserve/") //use http.StripPrefix?
 	name = strings.ToLower(name)
 	if strings.Contains(name, ".") {
 		http.Error(w, "no sub sub domains", http.StatusBadRequest)
@@ -83,11 +96,37 @@ func Reserve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if names[name] {
-		http.Error(w, "Already created "+name, http.StatusConflict)
+	names, err := listRecords(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fullname := "_dnslink." + name + "." + nb
+	if content, found := names[fullname]; found {
+		http.Error(w, fmt.Sprintf("Already created %s as %s", name, content), http.StatusConflict)
 		return
 	}
 
-	names[name] = true
+	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		http.Error(w, fmt.Errorf("can't getcloudflare client %w", err).Error(), http.StatusInternalServerError)
+		return
+	}
+	nbid, err := api.ZoneIDByName(nb)
+	if err != nil {
+		http.Error(w, fmt.Errorf("can't get zone %w", err).Error(), http.StatusInternalServerError)
+		return
+
+	}
+	resp, err := api.CreateDNSRecord(r.Context(), nbid, cloudflare.DNSRecord{
+		Name:    fullname,
+		Type:    "TXT",
+		Content: "dnslink=/ipfs/bafybeieenxnjdjm7vbr5zdwemaun4sw4iy7h4imlvvl433q6gzjg6awdpq",
+	})
+	if err != nil || !resp.Success {
+		http.Error(w, "Error reserving "+name, http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
 }
