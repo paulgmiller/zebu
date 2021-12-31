7fe18bb874bf670cc4e25259b1f1e6288fc86c1c
paul.miller@gmail.com <paul.miller@gmail.com>
2021-12-30 16:39:53 -0800

    start using the cloudflare api

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module paulgmiller/zebu
 go 1.15
 
 require (
+	github.com/cloudflare/cloudflare-go v0.29.0 // indirect
 	github.com/docker/docker v20.10.6+incompatible // indirect
 	github.com/driverzhang/golang-qrcode v0.0.0-20200306131207-34b6added23e // indirect
 	github.com/ethereum/go-ethereum v1.10.3
@@ -15,6 +16,7 @@ require (
 	github.com/libp2p/go-libp2p-core v0.8.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/moby/moby v20.10.6+incompatible
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/wealdtech/go-ens/v3 v3.4.5
 )
--- northbriton/main.go
+++ northbriton/main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
+	"time"
+
+	"github.com/cloudflare/cloudflare-go"
 )
 
 var names map[string]bool = map[string]bool{}
 
 func main() {
 
+	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		log.Fatalf("can't getcloudflare client %s", err)
+	}
+	nbid, err := api.ZoneIDByName("northbriton.net")
+	if err != nil {
+		log.Fatalf("can't get details %s", err)
+	}
+	fmt.Printf("trying to get records for %s\n", nbid)
+	records, err := api.DNSRecords(context.TODO(), nbid, cloudflare.DNSRecord{}) //Type: "TXT"
+	if err != nil {
+		log.Fatalf("can't get dns records %s", err)
+	}
+
+	for _, record := range records {
+		fmt.Printf(record.Name)
+	}
 	// Set routing rules
-	http.HandleFunc("/reserve/", Reserve)
-	http.HandleFunc("/list", List)
+	http.HandleFunc("/reserve/", withLogging(Reserve))
+	http.HandleFunc("/list", withLogging(List))
 
 	//Use the default DefaultServeMux.
-	err := http.ListenAndServe(":8000", nil)
+	err = http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,13 +48,22 @@ func main() {
 func List(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
-
+	log.Println("list request")
 	for name := range names {
 		fmt.Fprintln(w, name)
 	}
 }
 
-var domainregex = regexp.MustCompile("^[A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])?$")
+//https://github.com/gorilla/handlers
+func withLogging(h http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		h.ServeHTTP(rw, r) // serve the original request
+		log.Printf("%s %s: duration %d", r.RequestURI, r.Method, time.Since(start))
+	}
+}
+
+var domainregex = regexp.MustCompile(`^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?$`)
 
 func Reserve(w http.ResponseWriter, r *http.Request) {
 	fmt.Println()
