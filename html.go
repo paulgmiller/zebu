a1d3d4a9e40c79d26f244383e44b9c75c02fe91d
Paul Miller <pmiller@microsoft.com>
2022-06-27 02:56:13 +0000

    emed tempaltes

--- html.go
+++ html.go
@@ -1,66 +1,13 @@
 package main
 
-import "html/template"
+import (
+	"embed"
+	"html/template"
+)
 
-var indextmpl string = `<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>{{ .UserPublicName }}</title>
-	</head>
-	<body>
-		<div><a href="/user/{{ .UserPublicName }}">{{ .UserPublicName }}</a>(ask friends to follow {{ .UserPublicName }})</div>
-		<br />
-		<form action="/post" method="post" enctype="multipart/form-data">
-			<textarea name="post" rows="12" cols="100"></textarea>
-			<br/>
-			<input type="file" name="images" accept="image/*" multiple>
-			<br/>
-			<input type="submit" value="Submit">
-		</form>
-		<form action="/follow" method="post">
-			<textarea name="followee" rows="2" cols="64"></textarea>
-			<br/>
-			<input type="submit" value="Follow">
-		</form>
-		<br/>
-		{{range .Posts}}
-		<div>{{ .RenderedContent }}</div>
-		{{range .Images}}
-		<img src="/img/{{.}}" width="100"/>
-		{{end}}
-		<div><a href="/user/{{ .AuthorPublicName }}">{{ .AuthorPublicName }}</a> at {{ .Created }}</div>
-		<br />		
-        {{else}}
-        <div><strong>No Posts</strong></div>
-        {{end}}
-	</body>
-</html>`
+//go:embed templates
+var content embed.FS
 
-var registertmpl string = `<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Register public name</title>
-	</head>
-	<body>
-		<div>Claim a domain on northbriton.net</div>
-		<br />
-		<form action="/register" method="post">
-			<textarea name="publicname" rows="2" cols="64"></textarea>
-			<br/>
-			<input type="submit" value="Submit">
-		</form>
-		<br/>
-		<div>Todo let people claim an existing domain/ens and verify it</div>
-	</body>
-</html>`
-
-//https://pkg.go.dev/github.com/gin-gonic/gin#readme-build-a-single-binary-with-templates
 func loadTemplates() (*template.Template, error) {
-	t, err := template.New("index.tmpl").Parse(indextmpl)
-	if err != nil {
-		return nil, err
-	}
-	return t.New("register.tmpl").Parse(registertmpl)
+	return template.ParseFS(content, "templates/*.tmpl")
 }
