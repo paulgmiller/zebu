79a1726a09298163f042039af8a956b3e38fe92c
Paul Miller <pmiller@microsoft.com>
2022-12-23 01:08:27 +0000

    allow ens lookup with endpoint

--- resolve.go
+++ resolve.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -11,10 +10,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ipfs/go-dnslink"
+	"github.com/wealdtech/go-ens/v3"
 )
 
-//other options https://eth.link/
+//other options https://eth.link/ doesn't actually support direct address resolution?
 
 /*$ curl -H 'content-type: application/dns-json' 'https://eth.link/dns-query?type=TXT&name=wealdtech.eth'
 {
@@ -32,81 +33,45 @@ type txtRecord struct {
 	Data string `json:"data"`
 }
 
-type ethlinkResponse struct {
-	Answer []txtRecord
-}
-
-func ResolveEthLink(ensdomain string) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, "https://eth.link/dns-query?type=TXT&name="+ensdomain, nil)
-	if err != nil {
-		return "", err
-	}
-	req.Header.Add("content-type", "application/dns-json")
-	req.Header.Set("User-Agent", "zebu")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("bad status code %d", resp.StatusCode)
-	}
-	return parsEthLink(resp.Body)
-}
-
-func parsEthLink(body io.ReadCloser) (string, error) {
-	var elresp ethlinkResponse
-	if err := json.NewDecoder(body).Decode(&elresp); err != nil {
-		return "", err
-	}
-	for _, txt := range elresp.Answer {
-
-		parts := strings.Split(strings.Trim(txt.Data, "\""), "=")
-		if len(parts) == 2 && strings.EqualFold(parts[0], "contenthash") {
-			return parts[1], nil
-		}
-	}
-	return "", fmt.Errorf("no contenthash txt record found")
-}
-
 //https://github.com/cpacia/ens-lite seems ideally but go get fails
 //maybe because  github.com/ethereum/go-ethereum/contracts/ens moved to
 // use https://github.com/wealdtech/go-ens/tree/master/contracts instead
-/*
-func ResolveEns(ensdomain string) {
+
+var NoEthEndpoint = errors.New("ETHENDPOINT not defined")
+
+func ResolveEns(ensdomain string) (string, error) {
 	//obviusly need a light client but
 	ethendpoint := os.Getenv("ETHENDPOINT")
+	if ethendpoint == "" {
+		return "", NoEthEndpoint
+	}
+
 	client, err := ethclient.Dial(ethendpoint)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	// Resolve a name to an address
 	resolver, err := ens.NewResolver(client, ensdomain)
-
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	hash, err := resolver.Contenthash()
+	addr, err := resolver.Address()
 	if err != nil {
-		panic(err)
+		return "", err
+	}
+	/*hash, err := resolver.Contenthash()
+	if err != nil {
+		return "", err
 	}
 	readablehash, err := ens.ContenthashToString(hash)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	fmt.Printf("Content hash of %s is %s\n", ensdomain, readablehash)
+	log.Printf("Content hash of %s is %s\n", ensdomain, readablehash)*/
+	return addr.String(), nil
 
-	//usercid, err := ipfsShell.Resolve(*resolve)
-	//
-	//	link, err := dnslink.Resolve(*ensdomain)
- //		if err != nil {
-//			fmt.Println(err.Error())
-//		} else {
-//			fmt.Println(link)
-//	}
-
-}*/
+}
 
 func RegisterDNS(displayname, publicname string) (string, error) {
 	//see if this is already
