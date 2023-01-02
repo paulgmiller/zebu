b1afc7e3a6a3c945cb2baecfe7359cf163054c17
Paul Miller <pmiller@microsoft.com>
2023-01-02 17:05:45 +0000

    follow test

--- test/create_test.go
+++ test/create_test.go
@@ -8,7 +8,9 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 	"paulgmiller/zebu/zebu"
+	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -52,6 +54,46 @@ func TestPost(t *testing.T) {
 	}
 	unr.Sign(privatekey)
 	jbytes, err := json.Marshal(unr)
+	must(err, t, "marshal")
+	req, err = http.NewRequestWithContext(ctx, http.MethodPost, endpoint()+"/sign", bytes.NewReader(jbytes))
+	must(err, t, "req sign")
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = http.DefaultClient.Do(req)
+	must(err, t, "do sign")
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("bad status %d", resp.StatusCode)
+	}
+
+	//follow
+	followbody := &url.Values{}
+	followbody.Set("account", addr)
+	followbody.Set("followee", "johnwilkes.northbriton.net")
+	//todo: add image
+	req, err = http.NewRequestWithContext(ctx, http.MethodPost, endpoint()+"/follow", strings.NewReader(followbody.Encode()))
+	must(err, t, "req")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err = http.DefaultClient.Do(req)
+	must(err, t, "do")
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("bad status %d", resp.StatusCode)
+	}
+	body, err = io.ReadAll(resp.Body)
+	must(err, t, "read")
+	fmt.Println(string(body))
+
+	var unr2 zebu.UserNameRecord
+	err = json.Unmarshal(body, &unr2)
+	//err = json.NewDecoder(resp.Body).Decode(resp.Body)
+	must(err, t, "decode")
+	if unr.PubKey != addr {
+		t.Fatalf("bad pub key %s", unr2.PubKey)
+	}
+	unr2.Sign(privatekey)
+	if !unr2.Validate() {
+		t.Fatalf("bad signature, %s", unr2.Signature)
+	}
+	jbytes, err = json.Marshal(unr2)
 	must(err, t, "marshal")
 	req, err = http.NewRequestWithContext(ctx, http.MethodPost, endpoint()+"/sign", bytes.NewReader(jbytes))
 	must(err, t, "req sign")
